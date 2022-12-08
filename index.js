c5cbf7247ca2dfcfff635fda68e06f7b09a676ac
kuouu <erickuo5124@gmail.com>
2022-12-08 17:47:57 +0800

    first commit

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const bodyParser = require('body-parser');
+const dbo = require('./db');
 require('dotenv').config()
+
+const PORT = process.env.PORT || 3000;
 
 app.use(cors())
 app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(require('./routes'));
 
 
+dbo.connectToServer(function (err) {
+  if (err) {
+    console.error(err);
+    process.exit();
+  }
 
-
-
-const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+  // start the Express server
+  app.listen(PORT, () => {
+    console.log('Your app is listening on port ' + PORT)
+  })
+});
