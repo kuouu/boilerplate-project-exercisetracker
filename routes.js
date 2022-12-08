696e0785fca85887739a71889425ed74ae5da11b
kuouu <erickuo5124@gmail.com>
2022-12-08 19:11:10 +0800

    fix(route): add return to res

--- routes.js
+++ routes.js
@@ -24,9 +24,9 @@ recordRoutes.route('/api/users')
       .find({})
       .toArray(function (err, result) {
         if (err) {
-          res.status(400).send('Error fetching users!');
+          return res.status(400).send('Error fetching users!');
         } else {
-          res.json(result)
+          return res.json(result)
         }
       })
   })
@@ -38,9 +38,9 @@ recordRoutes.route('/api/users')
       .collection('users')
       .insertOne({ username: username }, (err, result) => {
         if (err) {
-          res.status(400).send('Error creating user!');
+          return res.status(400).send('Error creating user!');
         } else {
-          res.json({ username: username, _id: result.insertedId })
+          return res.json({ username: username, _id: result.insertedId })
         }
       })
   })
@@ -52,14 +52,14 @@ recordRoutes.route('/api/users/:_id/exercises')
     const userId = req.params._id
     const description = req.body.description
     const duration = Number(req.body.duration)
-    if (isNaN(duration)) res.json({ error: 'Duration must be a number' })
+    if (isNaN(duration)) return res.json({ error: 'Duration must be a number' })
 
     let date
     if (req.body.date === '') date = new Date()
     else {
       date = new Date(req.body.date)
       if (isNaN(date))
-        res.json({ error: 'Date must be a valid date' })
+        return res.json({ error: 'Date must be a valid date' })
     }
     const dbConnect = dbo.getDb();
     date = date.toDateString()
@@ -77,9 +77,9 @@ recordRoutes.route('/api/users/:_id/exercises')
             date: date
           }, (err, result) => {
             if (err) {
-              res.status(400).send('Error creating exercise!');
+              return res.status(400).send('Error creating exercise!');
             } else {
-              res.json({
+              return res.json({
                 _id: result.insertedId,
                 username: user.username,
                 description: description,
@@ -102,7 +102,7 @@ recordRoutes.route('/api/users/:_id/logs')
     const limit = req.query.limit ? Number(req.query.limit) : 0
     findUser((err, user) => {
       if (err) {
-        res.status(400).send('Error finding user!');
+        return res.status(400).send('Error finding user!');
       } else {
         const dbConnect = dbo.getDb();
         dbConnect
@@ -118,9 +118,9 @@ recordRoutes.route('/api/users/:_id/logs')
           .limit(limit)
           .toArray(function (err, result) {
             if (err) {
-              res.status(400).send('Error fetching exercises!');
+              return res.status(400).send('Error fetching exercises!');
             } else {
-              res.json({
+              return res.json({
                 _id: userId,
                 username: user.username,
                 count: result.length,
