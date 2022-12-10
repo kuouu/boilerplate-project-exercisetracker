a5a0d4e81d3fd20b251ea9c5cdfa4dfa7f4f7a90
kuouu <erickuo5124@gmail.com>
2022-12-10 17:56:28 +0800

    fix(api): fix connection error

--- api/index.js
+++ api/index.js
@@ -1,16 +1,19 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+app.use(cors());
+app.use(express.static('public'));
+app.get('/', (_, res) => {
+  res.sendFile(__dirname + '/views/index.html');
+});
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(require('./routes'));
 
 app.get('/api', (req, res) => {
-  res.setHeader('Content-Type', 'text/html');
-  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
   res.end('Hello!');
-});
-
-app.get('/api/item/:slug', (req, res) => {
-  const { slug } = req.params;
-  res.end(`Item: ${slug}`);
 });
 
 module.exports = app;
--- index.js
+++ index.js
@@ -1,29 +1,8 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const bodyParser = require('body-parser');
-const dbo = require('./db');
-require('dotenv').config()
+const app = require('./api')
 
 const PORT = process.env.PORT || 3000;
 
-app.use(cors())
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
-});
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(require('./routes'));
-
-
-dbo.connectToServer(function (err) {
-  if (err) {
-    console.error(err);
-    process.exit();
-  }
-
-  // start the Express server
-  app.listen(PORT, () => {
-    console.log('Your app is listening on port ' + PORT)
-  })
-});
+// start the Express server
+app.listen(PORT, () => {
+  console.log('Your app is listening on port ' + PORT)
+})
